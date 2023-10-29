43236a8ece8d0368328061724ae09e846551ab6c
ArifCahyaDito <acahyadito@gmail.com>
2023-10-29 08:12:57 +0700

    first commit

---
+++ UTS/Arif Cahya Dito_201011401744_07TPLP013_Database_Smartphone.sql
@@ -0,0 +1,22 @@
+-- -- CREATE TABLE smartphone (
+-- --     nama_smartphone VARCHAR(100),
+-- --     ram_gb INT,
+-- --     rom_gb INT,
+-- --     prosesor VARCHAR(100),
+-- --     baterai_mah INT,
+-- --     harga_rp DECIMAL(10, 2)
+-- -- );
+
+-- INSERT INTO smartphone (nama_smartphone, ram_gb, rom_gb, prosesor, baterai_mah, harga_rp) VALUES
+--     ('Samsung Galaxy A01', 2, 16, 'Qualcomm Snapdragon 439', 3000, 1499000),
+--     ('Samsung Galaxy M01', 3, 32, 'Qualcomm Snapdragon 439', 4000, 1999000),
+--     ('Xiaomi Redmi 9C', 3, 64, 'Mediatek Helio G35', 5000, 1899000),
+--     ('Xiaomi Redmi 9', 3, 32, 'Mediatek Helio G80', 5020, 1799000),
+--     ('Realme C11', 2, 32, 'Mediatek Helio G35', 5000, 1499000),
+--     ('Realme C21', 3, 32, 'Mediatek Helio G35', 5000, 1699000),
+--     ('Oppo A15', 3, 32, 'Mediatek Helio P35', 4230, 1699000),
+--     ('Oppo A53', 4, 64, 'Qualcomm Snapdragon 460', 5000, 2299000),
+--     ('Vivo Y12s', 3, 32, 'Mediatek Helio P35', 5000, 1699000),
+--     ('Vivo Y20', 4, 64, 'Qualcomm Snapdragon 460', 5000, 2099000);
+
+select *from smartphone;
